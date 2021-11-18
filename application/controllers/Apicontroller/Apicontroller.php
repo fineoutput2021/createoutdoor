3810653ff5f7567efaa5b0eed7eadf271c5f7514
suryafineoutput <happu.panwar019@gmail.com>
2021-11-18 19:04:09 +0530

    api update

--- application/controllers/Apicontroller/Apicontroller.php
+++ application/controllers/Apicontroller/Apicontroller.php
@@ -4693,7 +4693,7 @@ $txn_id=$t;
 
                                         $this->db->select('*');
                                         $this->db->from('tbl_products');
-                                        $this->db->where('category',$category_id);
+                                        $this->db->like('category',$category_id);
                                         $product_data= $this->db->get();
 
                                         // print_r($product_data);
